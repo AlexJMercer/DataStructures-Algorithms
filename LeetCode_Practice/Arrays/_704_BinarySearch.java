6412f16b2a9571f4ac2856165688aa046e5c0eab
Tathagata Guha Ray <nguharay71@gmail.com>
2022-06-29 17:05:26 +0530

    New Programs Added

---
+++ LeetCode_Practice/Arrays/_704_BinarySearch.java
@@ -0,0 +1,39 @@
+package LeetCode_Practice.Arrays;
+
+import java.util.Scanner;
+
+public class _704_BinarySearch {
+    static int search(int[] nums, int target) {
+        int low, mid, high;
+        low = 0;
+        high = nums.length-1;
+        
+        while (low <= high) {
+            mid = (low+high)/2;
+            if(target > nums[mid])
+                low = mid+1;
+            else if (target < nums[mid])
+                high = mid-1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter size of array : ");
+        int n = input.nextInt();
+        
+        int[] nums = new int[n];
+        System.out.print("Enter elements : ");
+        for (int i=0; i<n; i++)
+            nums[i] = input.nextInt();
+        
+        System.out.print("Enter target to find : ");
+        int target = input.nextInt();
+        System.out.println(search(nums, target));
+        input.close();
+    }
+}
---
+++ LeetCode_Practice/Miscellaneous/_278_FirstBadVersion.java
@@ -0,0 +1,35 @@
+package LeetCode_Practice.Miscellaneous;
+
+import java.util.Scanner;
+
+public class _278_FirstBadVersion {
+    static int bad;
+
+    static boolean isBadVersion(int version) {
+        return (version >= bad);
+    }
+
+    static int firstBadVersion(int n) {
+        int low = 1, mid, high = n;
+        
+        while (low < high) {
+            mid = low+(high-low)/2;
+            
+            if(isBadVersion(mid))
+                high = mid;
+            else
+                low = mid+1;
+        }
+        return high;
+    }
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Enter total number of versions : ");
+        int n = x.nextInt();
+        System.out.print("Enter first bad version : ");
+        bad = x.nextInt();
+
+        System.out.println("First bad version is " + firstBadVersion(n));
+        x.close();
+    }
+}
