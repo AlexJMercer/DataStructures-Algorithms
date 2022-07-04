42bbb0e2a5774850ab16b257fd99e3baf75d4b1e
Tathagata Guha Ray <nguharay71@gmail.com>
2022-07-04 07:18:09 +0530

    Problems on Bit Manipulation added

---
+++ LeetCode_Practice/BitManipulation/_136_SingleNumber.java
@@ -0,0 +1,27 @@
+package LeetCode_Practice.BitManipulation;
+
+import java.util.Scanner;
+
+public class _136_SingleNumber {
+    static int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i : nums) {
+            res ^= i;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        System.out.print("Enter size of array : ");
+        int n = x.nextInt();
+        
+        int[] nums = new int[n];
+        System.out.print("Enter elements : ");
+        for (int i=0; i<n; i++)
+            nums[i] = x.nextInt();
+        System.out.println("Output : " + singleNumber(nums));
+        x.close();
+    }
+}
---
+++ LeetCode_Practice/BitManipulation/_190_ReverseBits.java
@@ -0,0 +1,25 @@
+package LeetCode_Practice.BitManipulation;
+
+import java.util.Scanner;
+
+public class _190_ReverseBits {
+    static int reverseBits (int n) {
+        int rev = 0;
+        for (int i=0; i<32; i++) {
+            rev <<= 1;
+            if ((n & 1) == 1) {
+                rev++;
+            }
+            n >>= 1;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = x.nextInt();
+        System.out.print("Output : " + reverseBits(n));
+        x.close();
+    }
+}
