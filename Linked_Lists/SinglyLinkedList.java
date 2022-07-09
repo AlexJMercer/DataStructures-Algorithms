aff8e598d23dc183d6cc29ea785dbc261d55ff03
Tathagata Guha Ray <nguharay71@gmail.com>
2022-07-09 22:57:29 +0530

    Added deletion and string functions

--- Linked_Lists/SinglyLinkedList.java
+++ Linked_Lists/SinglyLinkedList.java
@@ -1,5 +1,7 @@
 package Linked_Lists;
 
+import java.util.Scanner;
+
 /**
  * Node class declares a Linked List.
  * Getters & Setters implemented.
@@ -57,6 +59,19 @@ public class SinglyLinkedList {
         return head;
     }
 
+    public int length() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return (length == 0);
+    }
+
+    public void clear() {
+        head = null;
+        length = 0;
+    }
+
     public synchronized void insertAtBegin(Node node) {
         node.setNext(head);         // Sets the new node as the head
         head = node;
@@ -101,7 +116,123 @@ public class SinglyLinkedList {
         length++;
     }
 
-    public static void main(String[] args) {
+    public synchronized Node removeFromBegin() {
+        Node node = head;
+        if (node != null) {
+            head = node.getNext();
+            node.setNext(null);
+        }
+        return node;
+    }
+
+    public synchronized Node getLast() {
+        if (head == null) {
+            return null;
+        }
+        if (head.getNext() == null) {
+            return head;
+        }
+        Node node = head.getNext();
+        while (node.getNext() != null) {
+            node = node.getNext();
+        }
+        return node;
+    }
+
+    public synchronized Node removeFromEnd() {
+        if (head == null)
+            return null;
+        Node p = head, q = null, next = head.getNext();
+        if (next == null) {
+            head = null;
+            length--;
+            return p;
+        }
+        while ((next = p.getNext()) != null) {
+            q = p;
+            p = next;
+        }
+        q.setNext(null);
+        length--;
+        return p;
+    }
+
+    public synchronized void removeMatched(Node node) {
+        if (head == null)
+            return;
+        if (node.equals(head)) {
+            head = head.getNext();
+            length--;
+            return;
+        }
+        Node p = head, q = null;
+        while ((q = p.getNext()) != null) {
+            if (node.equals(q)) {
+                p.setNext(q.getNext());
+                length--;
+                return;
+            }
+            p = q;
+        }
+    }
+
+    public void remove(int pos) {
+        if (pos == 0)
+            pos = 0;
+        if (pos >= length)
+            pos = length-1;
+        if (head == null)
+            return;
         
+        if (pos == 0)
+            head = head.getNext();
+        else {
+            Node node = head;
+            for (int i=1; i < pos; i++) {
+                node = node.next;
+            }
+            node.setNext(node.getNext().getNext());
+        }
+        length--;
+    }
+
+    public int getPosition(int data) {
+        Node node = head;
+        int pos = 0;
+        while (node != null) {
+            if (node.data == data)
+                return pos;
+            pos++;
+            node = node.getNext();
+        }
+        return -1;
+    }
+
+    public String toString() {
+        String res = "[";
+        if (head == null) {
+            return res + "]";
+        }
+        res = res + head.getData();
+        Node temp = head.getNext();
+        while (temp != null) {
+            res = res + "," + temp.getData();
+            temp = temp.getNext();
+        }
+        return res + "]";
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        Scanner x = new Scanner(System.in);
+        
+        System.out.print("Enter number of elements to insert : ");
+        int n = x.nextInt();
+        for (int i=0; i < n; i++) {
+            list.insert(x.nextInt(), i+1);
+        }
+              
+
+        x.close();
     }
 }
