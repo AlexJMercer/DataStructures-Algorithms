294593f3f55a0f091947136f3bc5a7c311b7adc9
Tathagata Guha Ray <nguharay71@gmail.com>
2022-07-19 01:09:04 +0530

    new solution

---
+++ LeetCode_Practice/C++_Solutions/Mathematics/_7_ReverseInteger.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+        int reverse(int x)
+        {
+            int rev = 0;
+            while (x != 0) {
+                int pop = x%10;
+                x /= 10;
+                if (rev > INT32_MAX/10 || (rev == INT32_MAX/10 && pop > 7))
+                    return 0;
+                if (rev < INT32_MIN/10 || (rev == INT32_MIN/10 && pop < -8))
+                    return 0;
+                rev = rev*10 + pop;
+            }
+            return rev;
+        }
+};
