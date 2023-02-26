e56a798269394ee873d2b6e275af45868d1c8ec0
Tathagata Guha Ray <nguharay71@gmail.com>
2023-02-27 03:39:38 +0530

    updated code

--- LeetCode_Practice/C++_Solutions/Strings/_8_StringtoInteger.cpp
+++ LeetCode_Practice/C++_Solutions/Strings/_8_StringtoInteger.cpp
@@ -5,32 +5,26 @@ using namespace std;
 class Solution {
 public:
     int myAtoi(string s) {
-        int i=0, sign = 1;
-        long long int num = 0;
+        double num = 0;
+        int i=0;
+        bool sign = 1;
         while (s[i] == ' ')
             i++;
-
+        
         if (s[i] == '+' || s[i] == '-')
         {
             if (s[i] == '-')
-                sign = -1;
+                sign = 0;
             i++;
         }
 
-        while (i < s.length())
-        {
-            if (s[i] < '0' || s[i] > '9')
-                break;    
-            
-            num = num*10 + (int)(s[i] - '0');
+        while(i < s.size() && s[i] >= '0' && s[i] <= '9'){
+            num = num*10 + (s[i]-'0');
             i++;
         }
-
-        if (num > INT_MAX)
-            return sign*INT_MAX;
-        else if (num < INT_MIN)
-            return sign*INT_MIN;
-        else
-            return sign*num;
+        num = (sign) ? num : -num;
+        num = (num > INT_MAX) ? INT_MAX : num;
+        num = (num < INT_MIN) ? INT_MIN : num;
+        return num;
     }
 };
